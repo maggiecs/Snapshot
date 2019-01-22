c1729b87ddb221847d50ccf053ea4cdadfc84556
Maggie Chen <mchen1415@gmail.com>
2019-01-22 17:09:13 -0500

    Add util and action for user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    return nil unless session[:session_token]
     @current_user ||= User.find_by(session_token: session[:session_token]);
   end
 
@@ -15,9 +14,8 @@ class ApplicationController < ActionController::Base
   end
 
   def login(user)
-    user.reset_session_token!
-    session[:session_token] = user.session_token
     @current_user = user
+    session[:session_token] = user.reset_session_token!
   end
 
   def logout
