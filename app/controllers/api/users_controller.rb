07d6c2d540b5e370dc01c42d064a306513b7b34b
Maggie Chen <mchen1415@gmail.com>
2019-01-22 14:21:29 -0500

    Create basic skeleton

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,32 @@
+class Api::UsersController < ApplicationRecord
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render "api/users/show"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      render "api/users/show"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+end
