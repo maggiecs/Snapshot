e2f438f6f9a9b25229940156240b96aee94fce83
App Academy Student <aa@Apps-Mac-mini.local>
2019-01-22 21:42:05 -0500

    Add login and signout functionality

---
+++ frontend/components/greeting/greeting.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Greeting = ({currentUser, logout}) => {
+  const sessionLinks = () => {
+    return (
+      <nav>
+        <Link to="/login">Login</Link>
+        <br/>
+        <Link to="/signup">Sign up!</Link>
+      </nav>
+    )
+  };
+
+  const personalGreeting = () => (
+    <div>
+      <h2 className="header-name">Hi, {currentUser.username}!</h2>
+      <button className="header-button" onClick={logout}>Log Out</button>
+    </div>
+  );
+
+  return currentUser ? personalGreeting() : sessionLinks();
+};
+
+export default Greeting;
--- frontend/snapshot.jsx
+++ frontend/snapshot.jsx
@@ -2,27 +2,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //Components
-// import Root from './components/root';
+import Root from './components/root';
 import configureStore from './store/store';
 //TESTING (DELETE EVENTUALLY)
 import { signup, login, logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // let store;
-  // if (window.currentUser) {
-  //   const preloadedState = {
-  //     session: { id: window.currentUser.id },
-  //     entities: {
-  //       users: { [window.currentUser.id]: window.currentUser }
-  //     }
-  //   };
-    // store = configureStore(preloadedState);
-    // delete window.currentUser;
-  // } else {
-    // store = configureStore();
-  // }
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      session: { id: window.currentUser.id },
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
-  const store = configureStore();
   window.login = login;
   window.signup = signup;
   window.logout = logout;
@@ -30,6 +29,5 @@ document.addEventListener('DOMContentLoaded', () => {
   window.dispatch = store.dispatch;
   
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Snapshot!</h1>, root);
-  // ReactDOM.render(<Root store={store} />, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
