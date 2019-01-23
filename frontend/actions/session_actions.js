218afc4bf243cf6c7288e856bee1f1823d48da70
Maggie <mchen1415@gmail.com>
2019-01-23 09:49:05 -0500

    Add functionality to prevent persisted errors

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -3,11 +3,12 @@ import * as SessionApiUtil from "../util/session_api_util";
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+export const REMOVE_SESSION_ERRORS = "REMOVE_SESSION_ERRORS";
 
 export const login = (user) => {
   return dispatch => {
     return SessionApiUtil.login(user).then(user => dispatch(
-      receiveCurrentUser(user)), errors => dispatch(receiveErrors(errors)));
+      receiveCurrentUser(user)), errors => dispatch(receiveErrors(errors.responseJSON)));
   };
 };
 
@@ -42,6 +43,12 @@ const receiveErrors = (errors) => {
   return {
     type: RECEIVE_SESSION_ERRORS,
     errors
+  };
+};
+
+export const removeErrors = () => {
+  return {
+    type: REMOVE_SESSION_ERRORS,
   };
 };
 
