280f1fe9e709c0914c510213b3e1504348579448
Maggie <mchen1415@gmail.com>
2019-01-23 23:41:14 -0500

    Create seperate page for splash page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -6,6 +6,7 @@ import { Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
 import SignUpFormContainer from './session_form/signup_form_container';
 import LogInFormContainer from './session_form/login_form_container';
+import SplashFormContainer from './session_form/splash_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
@@ -15,7 +16,8 @@ const App = () => (
     </header>
     <Switch>
       <AuthRoute exact path="/login" component={LogInFormContainer} />
-      <AuthRoute exact path="/" component={SignUpFormContainer} />
+      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+      <AuthRoute exact path="/" component={SplashFormContainer} />
     </Switch>
   </div>
 );
