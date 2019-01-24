4b67f7721f0ca478bbb2f0bb3abbf896cc75acf7
Maggie <mchen1415@gmail.com>
2019-01-24 10:47:45 -0500

    Fix application css file

--- frontend/util/post_api_util.js
+++ frontend/util/post_api_util.js
@@ -1,10 +1,28 @@
 export const fetchPosts = () => {
   return $.ajax({
     method: "GET",
+    url: 
   });
 };
 
 export const fetchPost = () => {
+  return {
+    url: "/api/users/:id"
+  };
+};
+
+export const createPost = () => {
+  return {
+    url: "/api/users"
+  };
+};
+
+export const updatePost = () => {
+  return {
+
+  };
+};
+export const deletePost = () => {
   return {
 
   };
