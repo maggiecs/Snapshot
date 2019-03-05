5d0243c2bc34d6a4d99a79549e356cf1018cba90
Maggie <mchen1415@gmail.com>
2019-03-05 13:54:12 -0500

    Update Angellist spelling

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <ul className="login-bottom-links">
           <li><a href="https://github.com/maggiecs">GITHUB</a></li>
           <li><a href="https://www.linkedin.com/in/maggie-chen1">LINKEDIN</a></li>
-          <li><a href="https://angel.co/maggie-chen-11">ANGELIST</a></li>
+          <li><a href="https://angel.co/maggie-chen-11">ANGELLIST</a></li>
           <li><a href="http://maggiechen.me/">ABOUT MAGGIE</a></li>
         </ul>
       </nav>
